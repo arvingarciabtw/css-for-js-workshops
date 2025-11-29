6aa45813e04895f738934ff4fdc2d988f4ba9426
Arvin Garcia <arvin@arvingarcia.com>
2025-11-29 20:14:51 +0800

    feat: implement large progress bar

--- module-3-mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ module-3-mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  large: {
+    "--padding": "4px",
+    "--height": "24px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  const Wrapper = styled.div`
+    padding: var(--padding);
+    width: 370px;
+    height: var(--height);
+    background-color: ${COLORS.transparentGray15};
+    border-radius: 8px;
+    box-shadow: 2px 2px 1px ${COLORS.transparentGray35} inset;
+  `;
+
+  const NormalProgress = styled.div`
+    width: ${value}%;
+    height: 100%;
+    background-color: ${COLORS.primary};
+    border-radius: 4px 0 0 4px;
+  `;
+
+  const RoundedProgress = styled(NormalProgress)`
+    border-radius: 4px 4px 4px 4px;
+  `;
+
+  let Progress;
+  if (value >= 99) {
+    Progress = RoundedProgress;
+  } else {
+    Progress = NormalProgress;
+  }
+
+  return (
+    <>
+      <Wrapper style={styles}>
+        <Progress></Progress>
+      </Wrapper>
+    </>
+  );
 };
 
 export default ProgressBar;
