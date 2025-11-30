6aa7d9d9806929daa02703ee18fd2a57193fbee4
Arvin Garcia <arvin@arvingarcia.com>
2025-11-30 12:54:03 +0800

    feat: implement select component

--- module-3-mini-component-library/src/components/Select/Select.js
+++ module-3-mini-component-library/src/components/Select/Select.js
@@ -1,18 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ id, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect id={id} value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const NativeSelect = styled.select`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+`;
+
+const PresentationalBit = styled.div`
+  padding: 12px 16px;
+  padding-right: 52px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: ${16 / 16}rem;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 2px solid ${COLORS.primary};
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+`;
 
 export default Select;
